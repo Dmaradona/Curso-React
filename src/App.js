bdd68ce8b6bf83124dc330e6aabf38e99abb6fda
Diego Krause <diego.diegokrause.krause64@gmail.com>
2020-02-24 13:40:08 -0300

    Primeiros Passos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function ComponenteFuncional() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
+}
+
+class App extends React.Component{
+
+  state = {
+    nome : ''
+  }
+
+  constructor(){
+    super()
+    this.modificarNome = this.modificarNome.bind(this)
+  }
+
+  modificarNome(event){ /* ESTE MODELO DE FUNÇÃO PRECISA FAZER O .BIND() NO CONSTRUCTO */
+    this.setState({
+      nome : event.target.value
+    })
+  }
+
+  /*modificarNome2 = (event) => { // ESTE MODELO DE FUNÇÃO NÃO PRECISA FAZER O .BIND() "ARROWFUNCTION" É MAIS FÁCIL
+    this.setState({
+      nome : event.target.value
+    })
+  }*/
+
+  criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano"]
+    // A FUNÇÃO MAP PERCORRE O ARRAY E RETORNA A ESTRUTURA DETERMINADA PARA CADA ITEM (NÃO PRECISA DE RETURN NESSA ARROW FUNC POR SER EXPLICITO)
+    const comboBoxOptions = opcoes.map( opcao => <option>{ opcao }</option> )
+
+    return(
+      <select>
+        { comboBoxOptions }
+      </select>
+    )
+
+  }
+
+    /* DENTRO DO RENDER SÓ PODE RETORNAR UM NÓ OU UM AGRUPADO EM UMA DIV OU USAR A NOMENCLATURA <> QUE AÍ ENTENDE-SE QUE É UM GRUPO DE COMPONENTES*/
+  render(){
+    /* OUTRA FORMA DE CRIAR COMPONENTE */
+    const MeuComboBox = () => this.criaComboBox()
+
+    return(
+    <> 
+      <input type="text" value={ this.state.nome } onChange={ this.modificarNome } />
+      <h1>Hello { this.state.nome }</h1>
+      { this.criaComboBox() }
+      <MeuComboBox></MeuComboBox>
+    </>
+    )
+  }
 }
 
 export default App;
